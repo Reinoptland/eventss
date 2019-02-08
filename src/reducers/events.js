cf127e10d7291686600bd23a751faacbf412ec5f
reinoptland <reinoptland@gmail.com>
2019-02-08 14:57:38 +0100

    Handle EVENT_CREATE_SUCCESS in reducer

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,9 +1,11 @@
-import { EVENTS_FETCHED } from '../actions/events.js'
+import { EVENTS_FETCHED, EVENT_CREATE_SUCCESS } from '../actions/events.js'
 
 export default (state = [], action) => {
     switch (action.type) {
         case EVENTS_FETCHED:
             return action.events
+        case EVENT_CREATE_SUCCESS:
+            return [...state, action.event]
     
         default:
             return state
