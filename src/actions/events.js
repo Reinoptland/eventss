8571ffba2f3b75ef11705ccebefe0a40a45e1cb3
reinoptland <reinoptland@gmail.com>
2019-02-08 15:24:58 +0100

    Add action creator loadEvent

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,6 +1,7 @@
 import request from 'superagent'
 
 export const EVENTS_FETCHED = 'EVENTS_FETCHED'
+export const EVENT_FETCHED = 'EVENT_FETCHED'
 export const EVENT_CREATE_SUCCESS = 'EVENT_CREATE_SUCCESS'
 
 const baseUrl = 'http://localhost:4000'
@@ -8,6 +9,11 @@ const baseUrl = 'http://localhost:4000'
 const eventsFetched = events => ({
   type: EVENTS_FETCHED,
   events
+})
+
+const eventFetched = event => ({
+  type: EVENT_FETCHED,
+  event
 })
 
 export const loadEvents = () => (dispatch, getState) => {
@@ -19,6 +25,14 @@ export const loadEvents = () => (dispatch, getState) => {
     .then(response => {
       // dispatch an EVENTS_FETCHED action that contains the events
       dispatch(eventsFetched(response.body))
+    })
+    .catch(console.error)
+}
+
+export const loadEvent = (id) => (dispatch) => {
+  request(`${baseUrl}/events/${id}`)
+    .then(response => {
+      dispatch(eventFetched(response.body))
     })
     .catch(console.error)
 }
